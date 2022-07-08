2106102ea704927d3074291ee14de009e9912fc9
Matheus S <matheussouza4555@gmail.com>
2022-07-08 11:10:52 -0300

    Atualizado nome das variaveis sprite groups

--- versao_final/States/Game.py
+++ versao_final/States/Game.py
@@ -25,9 +25,9 @@ class Game(State):
     def __init__(self):
         State.__init__(self)
 
-        self.jogador = pg.sprite.GroupSingle()
-        self.attacks = pg.sprite.Group()
-        self.jogador.add(Player(start_pos=(200, 200)))
+        self.player_sprite_group = pg.sprite.GroupSingle()
+        self.attack_sprite_group = pg.sprite.Group()
+        self.player_sprite_group.add(Player(start_pos=(200, 200)))
 
         self.current_room = Room()
 
@@ -44,60 +44,60 @@ class Game(State):
                 self.quit = True
 
     def handle_collisions(self):
-        for attack_colission in pg.sprite.spritecollide(self.jogador.sprite, self.attacks, True, pg.sprite.collide_mask):
-            self.jogador.sprite.damage_taken(damage = attack_colission.damage)
+        for attack_colission in pg.sprite.spritecollide(self.player_sprite_group.sprite, self.attack_sprite_group, True, pg.sprite.collide_mask):
+            self.player_sprite_group.sprite.damage_taken(damage = attack_colission.damage)
 
-            if self.jogador.sprite.health <= 0:
+            if self.player_sprite_group.sprite.health <= 0:
                 self.next = "game_over"
                 self.done = True
 
-        #colisao jogador parede
+        #colisao player_sprite_group parede
 
 
-        #colisao jogador inimigo
+        #colisao player_sprite_group inimigo
 
 
         # colisao inimigo ataque
 
-        #colisao jogador porta
+        #colisao player_sprite_group porta
 
     def handle_keys(self, keys):
         super().handle_keys(keys)
 
-        #movimento jogador
+        #movimento player_sprite_group
         if keys[pg.K_w]:
-            self.jogador.sprite.rect.move_ip((0, -3))
+            self.player_sprite_group.sprite.rect.move_ip((0, -3))
         
         elif keys[pg.K_s]:
-            self.jogador.sprite.rect.move_ip((0, 3))
+            self.player_sprite_group.sprite.rect.move_ip((0, 3))
         
         elif keys[pg.K_a]:
-            self.jogador.sprite.rect.move_ip((-3, 0))
+            self.player_sprite_group.sprite.rect.move_ip((-3, 0))
         
         elif keys[pg.K_d]:
-            self.jogador.sprite.rect.move_ip((3, 0))
+            self.player_sprite_group.sprite.rect.move_ip((3, 0))
 
-        #ataque jogador
-        if pg.mouse.get_pressed()[0] and not self.jogador.sprite.in_cooldown:
+        #ataque player_sprite_group
+        if pg.mouse.get_pressed()[0] and not self.player_sprite_group.sprite.in_cooldown:
             mouse_pos = pg.mouse.get_pos()
-            player_pos = self.jogador.sprite.rect.center
+            player_pos = self.player_sprite_group.sprite.rect.center
             difference_pos = (player_pos[0] - mouse_pos[0], player_pos[1] - mouse_pos[1])
 
             if difference_pos[1] > 0 and difference_pos[1] > abs(difference_pos[0]):
-                self.attacks.add(self.jogador.sprite.attack('up'))
+                self.attack_sprite_group.add(self.player_sprite_group.sprite.attack('up'))
             
             elif difference_pos[1] < 0 and abs(difference_pos[1]) > abs(difference_pos[0]):
-                self.attacks.add(self.jogador.sprite.attack('down'))
+                self.attack_sprite_group.add(self.player_sprite_group.sprite.attack('down'))
             
             elif difference_pos[0] > 0 and difference_pos[0] > abs(difference_pos[1]):
-                self.attacks.add(self.jogador.sprite.attack('left'))
+                self.attack_sprite_group.add(self.player_sprite_group.sprite.attack('left'))
             
             elif difference_pos[0] < 0 and abs(difference_pos[0]) > abs(difference_pos[1]):
-                self.attacks.add(self.jogador.sprite.attack('right'))
+                self.attack_sprite_group.add(self.player_sprite_group.sprite.attack('right'))
 
     def update(self, screen):
-        self.attacks.update()
-        self.jogador.update()
+        self.attack_sprite_group.update()
+        self.player_sprite_group.update()
         self.draw(screen)
 
     def draw(self, screen):
@@ -106,6 +106,6 @@ class Game(State):
 
         pg.draw.rect(screen, (99, 23, 23), pg.Rect(0, 0, 1280, 180)) #HUD
 
-        self.attacks.draw(screen)
+        self.attack_sprite_group.draw(screen)
 
-        self.jogador.draw(screen)
+        self.player_sprite_group.draw(screen)
