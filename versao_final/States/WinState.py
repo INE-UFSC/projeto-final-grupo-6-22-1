cc2b4878525dc33141d892187b055add60a377d5
Matheus S <matheussouza4555@gmail.com>
2022-07-29 01:36:48 -0300

    Corrigido last score da tela de vitória

--- versao_final/States/WinState.py
+++ versao_final/States/WinState.py
@@ -13,13 +13,13 @@ class WinState(AbstractState):
 
         self.background = get_image('telas', 'TelaVitoria.png')
         self.background = pg.transform.smoothscale(self.background, (1280, 700))       
-        self.score = menu_font.render(str(self.score_controller.get_last_score()), True, (77, 0, 18))
+        self.score = menu_font.render("0", True, (77, 0, 18))
         self.rect_menu = pg.Rect(60, 560, 500, 125)
         #self.text_menu = menu_font.render('Retornar ao menu', True, (0, 0, 0))
 
 
     def startup(self):
-        print(self.score_controller.get_last_score())
+        self.score = pg.font.Font(pg.font.get_default_font(), 40).render(str(self.score_controller.get_last_score()), True, (77, 0, 18))
 
     def handle_events(self, event):
         if event.type == pg.QUIT:
